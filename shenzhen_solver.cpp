e5987b476476f0e47a03c1997e085c8d9420ed0f
djohnston <david.johnston@analogueinsight.com>
2026-05-17 12:09:36 -0700

    License header

--- shenzhen_solver.cpp
+++ shenzhen_solver.cpp
@@ -4,7 +4,17 @@
 // Run:    ./shenzhen_solver [-j N]
 //         -j N  : run N solver threads in parallel
 //
-
+// Copyright(c) David Johnston, 2026
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
 // Card encoding (uint8_t):
 //   0           : empty / sentinel
